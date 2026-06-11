a36d2b128191a46d8950bf395fd1bf6cbe30e9ec
kirilobelaev20-hash <_EMAIL__GITHUB>
2026-06-11 10:48:16 +0300

    23

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,3 @@
-function filterArray(numbers, value) {
-    const filteredNumbers = [];
-    for (const number of numbers) {
-        if (number > value) {
-            filteredNumbers.push(number);
-        }
-    }
-    return filteredNumbers;
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
 const sortByDescendingFriendCount = users => {
     return [...users].toSorted((a, b) => b.friends.length - a.friends.length);
 }
